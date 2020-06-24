a8e2f93b1ec18ec30572a100a17c789dea5f0861
Concrete18 <michaelericson19@gmail.com>
2020-06-24 15:16:05 -0400

    Fixed errors with sleep detection and auto timer reset detection.

--- Standing_Reminder.py
+++ Standing_Reminder.py
@@ -14,11 +14,11 @@ import os
 
 Config = configparser.RawConfigParser()
 
+
 def write_to_config():
     with open('Config.ini', 'w') as configfile:
         Config.write(configfile)
 
-Config.read('Config.ini')
 
 log_formatter = lg.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%m-%d-%Y %I:%M:%S %p')
 logFile = f'{os.getcwd()}\\Standing_Reminder.log'
@@ -56,16 +56,18 @@ global time_left, non_idle_time
 time_left = 0
 non_idle_time = 0
 
+
 def main_func():
-    with open('Config.ini', 'r') as configfile:
-        Config.read(configfile)
-        wait_till_idle = int(Config.get('Main', 'wait_till_idle'))
-        check_frequency = int(Config.get('Main', 'check_frequency'))
-        reminder_time = int(Config.get('Main', 'reminder_time'))
-        past_reminder_frequency = int(Config.get('Main', 'past_reminder_frequency'))*60
-        required_idle_time = int(Config.get('Main', 'reminder_time'))
-        non_idle_time = 0
-        last_run = dt.datetime.now()
+    Config.read('Config.ini')
+    wait_till_idle = int(Config.get('Main', 'wait_till_idle'))
+    check_frequency = int(Config.get('Main', 'check_frequency'))
+    reminder_time = int(Config.get('Main', 'reminder_time'))
+    past_reminder_frequency = int(Config.get('Main', 'past_reminder_frequency')) * 60
+    required_idle_time = int(Config.get('Main', 'required_idle_time'))
+    notification_popup = int(Config.get('Main', 'notification_popup'))
+    non_idle_time = 0
+    last_run = dt.datetime.now()
+    print(f'Standing Reminder set with a {check_frequency} minute frequency and a idle detection set to {wait_till_idle} minutes.')
     logger.info(f'Standing Reminder set with a {check_frequency} minute frequency and a idle detection set to {wait_till_idle} minutes.')
     tray.Update(tooltip=f'Standing Reminder\nNext Reminder: {reminder_time} minutes.')
     while True:
@@ -75,26 +77,29 @@ def main_func():
             non_idle_time = 0
             logger.info('Computer is Idle')
             while idle_duration > wait_till_idle * 60:
+                idle_duration = get_idle_duration()
                 time.sleep(check_frequency * 60)
+            logger.info('Computer is no longer Idle')
         else:
-            time_difference = dt.datetime.now() - last_run
-            if dt.datetime.now() - last_run >= dt.timedelta(minutes=wait_till_idle):
-                logger.info(f'Computer was asleep for more then {wait_till_idle} minutes.\nResetting timer.')
             non_idle_time += 1
+            if dt.datetime.now() - last_run >= dt.timedelta(minutes=wait_till_idle):
+                non_idle_time = 0
+                logger.info(f'Computer was asleep for more then {wait_till_idle} minutes. Resetting timer.')
             time_left = reminder_time - non_idle_time
             tray.Update(tooltip=f'Standing Reminder\nNext Reminder: {time_left} minutes.')
-            last_run = dt.datetime.now()
-        print(f'PC has been used for {non_idle_time} minute(s).')
+        last_run = dt.datetime.now()
+        print(f'PC has been used for {non_idle_time} minute(s).    ', end="\r")
         if non_idle_time >= reminder_time:
-            #  Todo set showmessage to replace old one and switch to bubble message if possible.
             tray.Update(tooltip=f'Standing Reminder\nNext Reminder: {reminder_time} minutes.')
-            playsound('juntos.mp3')
-            tray.ShowMessage('Standing Reminder', f'PC has been used for {non_idle_time} minute(s).\nYou should stand up and stretch some.\n', time=10)
-            logger.info('You should stand up and stretch some.\n')
-            while idle_duration < required_idle_time:
+            if notification_popup == 0:
+                playsound('juntos.mp3')
+            logger.info(f'PC has passed active time limit of {reminder_time} minutes.')
+            while idle_duration < required_idle_time or non_idle_time > reminder_time:
                 idle_duration = get_idle_duration()
-                tray.ShowMessage('Standing Reminder', f'PC has been used for {non_idle_time} minute(s).\nStop using the computer for {required_idle_time} minutes to reset this reminder.\n', time=10)
+                if notification_popup == 1:
+                    tray.ShowMessage('Standing Reminder', f'PC has been used for {non_idle_time} minute(s).\nStop using the computer for {required_idle_time} minutes to reset this reminder.\n', time=10)
                 time.sleep(past_reminder_frequency)
+            logger.info(f'Inactivity Met - Resetting')
             non_idle_time = 0
 
 
